e07d4c5ac1bfc5c4a4c5d7838b003aa68d6fa1d8
yongjing <near711@outlook.com>
2020-06-30 22:05:49 +0800

    NBody modified

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -127,7 +127,7 @@ public class NBody {
 		StdOut.printf("%.2e\n", radius);
 
 		for (int i = 0; i < allPlanets.length; i++) {
-    	StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+			StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
     		allPlanets[i].xxPos, allPlanets[i].yyPos, allPlanets[i].xxVel,
     		allPlanets[i].yyVel, allPlanets[i].mass, allPlanets[i].imgFileName);
     	}
