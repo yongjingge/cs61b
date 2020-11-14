9011d2204a1157805438102305c4015fd2ba3573
yongjing <near711@outlook.com>
2020-11-14 14:33:31 +0800

    HW1 completed

--- hw1/synthesizer/AbstractBoundedQueue.java
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -11,7 +11,7 @@ public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T> {
     }
 //    public boolean isEmpty;
 //    public boolean isFull;
-    public abstract T peek();
-    public abstract T dequeue();
-    public abstract void enqueue(T x);
+//    public abstract T peek();
+//    public abstract T dequeue();
+//    public abstract void enqueue(T x);
 }
