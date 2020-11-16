314fef39b9ac27b67d2cf47e7fedbb13f5e61c56
yongjing <near711@outlook.com>
2020-11-16 22:18:44 +0800

    lab5 - Drawing A Single Hexagon completed, Drawing A Tesselation of Hexagons completed.

--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -13,8 +13,8 @@ import java.util.Random;
  */
 public class HexWorld {
     /* @Source: Josh Hug */
-    private static final int WIDTH = 50;
-    private static final int HEIGHT = 50;
+    private static final int WIDTH = 70;
+    private static final int HEIGHT = 70;
     private static final long SEED = 2873123;
     private static final Random RANDOM = new Random(SEED);
 
@@ -120,19 +120,115 @@ public class HexWorld {
         }
     }
 
+    /**
+     * Adds hexagons vertically at a given position with randomly assigned styles.
+     * @param world the world to draw on
+     * @param p a given position
+     * @param size the size of the hexagon
+     * @param num number of hexagons to be added
+     * @source airbust
+     */
+    public static void addHexaVertical (TETile[][] world, Position p, int size, int num) {
+        Position next = new Position(p.x, p.y); // starts from a given position
+        for (int i = 0; i < num; i += 1) {
+            addHexagon(world, next, size, randomTile());
+            next.y += 2 * size; // y pluses the height of the hexagon, which should be twice of its size.
+        }
+    }
+
+    /** Picks a RANDOM tile with a 33% change of being
+     *  a wall, 33% chance of being a flower, and 33%
+     *  chance of being empty space.
+     */
+    private static TETile randomTile() {
+        int tileNum = RANDOM.nextInt(6);
+        switch (tileNum) {
+            case 0: return Tileset.WALL;
+            case 1: return Tileset.FLOWER;
+            case 2: return Tileset.MOUNTAIN;
+            case 3: return Tileset.UNLOCKED_DOOR;
+            case 4: return Tileset.LOCKED_DOOR;
+            case 5: return Tileset.SAND;
+            default: return Tileset.NOTHING;
+        }
+    }
+
+    /**
+     * 19 hexagons should be vertically 3-4-5-4-3
+     * this helper method provides a way to calculate the appropriate number of hexagons
+     * to be added each time
+     * @param i given number of hexagons to be added
+     * @source airbust
+     */
+    private static int calNum (int i) {
+        int res = i + 3;
+        if (i > 2) {
+            res = 10 - res;
+        }
+        return res;
+    }
+
+    /**
+     * @source airbust
+     * @param size size of the hexagon
+     * @return
+     */
+    private static int xMove (int size) {
+        return 2 * size + xOffset(size, 0) - 1;
+    }
+
+    /**
+     * @source airbust
+     * @param size size of the hexagon
+     * @param i
+     * @return
+     */
+    private static int yMove (int size, int i) {
+        int res = size;
+        if (i > 2) {
+            return res;
+        }
+        return -res;
+    }
+
+    /**
+     * draw a tesselation of hexagons
+     * @source airbust
+     * @param world
+     * @param p
+     * @param size size of the hexagon
+     */
+    public static void drawTesselationHexagons (TETile[][] world, Position p, int size) {
+        Position next = new Position(p.x, p.y);
+        if (p.y < size * 2) {
+            throw new ArrayIndexOutOfBoundsException("Position of y should be no less than " + 2 * size);
+        }
+        int newX = xMove(size);
+        for (int i = 0; i < 5; i += 1) {
+            addHexaVertical(world, next, size, calNum(i));
+            next.x += newX;
+            next.y += yMove(size, i + 1);
+        }
+    }
+
     public static void main (String[] args) {
         TERenderer ter = new TERenderer();
         ter.initialize(WIDTH, HEIGHT);
         TETile[][] world = new TETile[WIDTH][HEIGHT];
-        Position p1 = new Position(3,7);
+        Position p1 = new Position(3, 8);
         Position p2 = new Position(11,22);
+        Position p3 = new Position(3,5);
         for (int i = 0; i < WIDTH; i += 1) {
             for (int j = 0; j < HEIGHT; j += 1) {
                 world[i][j] = Tileset.NOTHING;
             }
         }
-        addHexagon(world, p1, 3, Tileset.FLOWER);
-        addHexagon(world, p2, 4, Tileset.TREE);
+        // addHexagon(world, p1, 3, Tileset.FLOWER);
+        // addHexagon(world, p2, 4, Tileset.TREE);
+
+        // addHexaVertical(world, p1, 3, 5);
+
+        drawTesselationHexagons(world, p3, 4);
         ter.renderFrame(world);
     }
 }
