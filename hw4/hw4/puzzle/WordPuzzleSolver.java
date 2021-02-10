cb2185abc996e5d0a0eeb3a158541d8853a5d721
yongjing <near711@outlook.com>
2021-02-10 18:09:23 +0800

    hw4 -- solver added

--- hw4/hw4/puzzle/WordPuzzleSolver.java
+++ hw4/hw4/puzzle/WordPuzzleSolver.java
@@ -6,16 +6,19 @@ public class WordPuzzleSolver {
      * Test routine for your Solver class. Uncomment and run to test
      * your basic functionality.
      **********************************************************************/
-    /*public static void main(String[] args) {
-        String start = "cube";
-        String goal = "tubes";
+    public static void main(String[] args) {
+//        String start = "cube";
+//        String goal = "tubes";
+
+        String start = "horse";
+        String goal = "nurse";
 
         Word startState = new Word(start, goal);
         Solver solver = new Solver(startState);
 
         StdOut.println("Minimum number of moves = " + solver.moves());
         for (WorldState ws : solver.solution()) {
-            StdOut.println(ws);
+            StdOut.print(ws + " ");
         }
-    }*/
+    }
 }
