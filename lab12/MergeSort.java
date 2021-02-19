674abd75d6671a37cf3a40af71cc2a4ad0f1bc46
yongjing <near711@outlook.com>
2021-02-19 23:14:39 +0800

    Lab12 -- MergeSorted finished

--- lab12/MergeSort.java
+++ lab12/MergeSort.java
@@ -1,6 +1,7 @@
 import edu.princeton.cs.algs4.Queue;
 
 public class MergeSort {
+
     /**
      * Removes and returns the smallest item that is in q1 or q2.
      *
@@ -34,8 +35,16 @@ public class MergeSort {
     /** Returns a queue of queues that each contain one item from items. */
     private static <Item extends Comparable> Queue<Queue<Item>>
             makeSingleItemQueues(Queue<Item> items) {
-        // Your code here!
-        return null;
+        if (items.isEmpty() || items == null) {
+            return null;
+        }
+        Queue<Queue<Item>> queueOfQueues = new Queue<>();
+        for (Item single : items) {
+            Queue<Item> singleQueue = new Queue<>();
+            singleQueue.enqueue(single);
+            queueOfQueues.enqueue(singleQueue);
+        }
+        return queueOfQueues;
     }
 
     /**
@@ -53,14 +62,69 @@ public class MergeSort {
      */
     private static <Item extends Comparable> Queue<Item> mergeSortedQueues(
             Queue<Item> q1, Queue<Item> q2) {
-        // Your code here!
-        return null;
+        Queue<Item> res = new Queue<>();
+        while (! (q1.isEmpty() && q2.isEmpty())) {
+            res.enqueue(getMin(q1, q2));
+        }
+        return res;
     }
 
     /** Returns a Queue that contains the given items sorted from least to greatest. */
     public static <Item extends Comparable> Queue<Item> mergeSort(
             Queue<Item> items) {
-        // Your code here!
-        return items;
+
+        if (items.size() <= 1) {
+            return items;
+        }
+        Queue<Queue<Item>> queueOfQueues = makeSingleItemQueues(items);
+        Queue<Item> subQueue1 = new Queue<>();
+        Queue<Item> subQueue2 = new Queue<>();
+
+        // fill the subQueue1 with left half of the queueOfQueues
+        int queue1Range = queueOfQueues.size() / 2;
+        for (int i = 0; i < queue1Range; i += 1) {
+            Item itemToBeEnqueued = queueOfQueues.dequeue().dequeue();
+            subQueue1.enqueue(itemToBeEnqueued);
+        }
+
+        // fill the subQueue2 with items left
+        // note that the size of queueOfQueues has changed due to the previous dequeue operation
+        int queue2Range = queueOfQueues.size();
+        for (int i = 0; i < queue2Range; i += 1) {
+            Item itemToBeEnqueued = queueOfQueues.dequeue().dequeue();
+            subQueue2.enqueue(itemToBeEnqueued);
+        }
+
+        Queue<Item> subQueue1Sorted = mergeSort(subQueue1);
+        Queue<Item> subQueue2Sorted = mergeSort(subQueue2);
+        return mergeSortedQueues(subQueue1Sorted, subQueue2Sorted);
+
+    }
+
+    /**
+     * test-driven development
+     * @param args
+     */
+    public static void main(String[] args) {
+        Queue<Integer> queueOfNumbers = new Queue<>();
+        queueOfNumbers.enqueue(11);
+        queueOfNumbers.enqueue(1);
+        queueOfNumbers.enqueue(27);
+        queueOfNumbers.enqueue(35);
+        queueOfNumbers.enqueue(24);
+        Queue<Integer> afterMergeSort = mergeSort(queueOfNumbers);
+        System.out.println("The original queue of Integer is: " + queueOfNumbers.toString());
+        System.out.println("After MergeSort, the queue is: " + afterMergeSort.toString());
+
+        Queue<String> queueOfStrings = new Queue<>();
+        queueOfStrings.enqueue("this");
+        queueOfStrings.enqueue("is");
+        queueOfStrings.enqueue("a");
+        queueOfStrings.enqueue("queue");
+        queueOfStrings.enqueue("of");
+        queueOfStrings.enqueue("strings");
+        Queue<String> afterMergeSort2 = mergeSort(queueOfStrings);
+        System.out.println("The original queue of String is: " + queueOfStrings.toString());
+        System.out.println("After MergeSort, the queue is: " + afterMergeSort2.toString());
     }
 }
